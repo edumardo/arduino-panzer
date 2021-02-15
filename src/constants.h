d0b6dd5eb640d799b488d7bd895b518387b1c3c6
Eduardo Delgado Díaz <ediaz@ideaingenieria.es>
2021-02-16 00:02:34 +0100

    const instead define

--- src/constants.h
+++ src/constants.h
@@ -1,8 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define BAUD_RATE           57600
-#define LED_HULLMG_INTERVAL 100
-#define PS2_MIDDLE_VALUE    128
+const int LED_HULLMG_INTERVAL = 100;
+const int PS2_MIDDLE_VALUE    = 128;
 
 #endif
--- src/pinout.h
+++ src/pinout.h
@@ -2,21 +2,21 @@
 #define PINOUT_H
 
 /* PS2 pins */
-#define PS2_PIN_DAT     12
-#define PS2_PIN_CMD     11
-#define PS2_PIN_SEL     10
-#define PS2_PIN_CLK     13
-#define PS2_PRESSURES   true
-#define PS2_RUMBLE      true
+const int PS2_PIN_DAT    = 12;
+const int PS2_PIN_CMD    = 11;
+const int PS2_PIN_SEL    = 10;
+const int PS2_PIN_CLK    = 13;
+const bool PS2_PRESSURES = true;
+const bool PS2_RUMBLE    = true;
 
 /* TB6612FNG pins */
-#define TB_PWMA         3
-#define TB_AIN2         2
-#define TB_AIN1         4
-#define TB_STBY         5
-#define TB_BIN1         6
-#define TB_BIN2         7
-#define TB_PWMB         9
+const int TB_PWMA = 3;
+const int TB_AIN2 = 2;
+const int TB_AIN1 = 4;
+const int TB_STBY = 5;
+const int TB_BIN1 = 6;
+const int TB_BIN2 = 7;
+const int TB_PWMB = 9;
 
 /* Led hull MG pin */
 #define LED_HULLMG_PIN  A2
