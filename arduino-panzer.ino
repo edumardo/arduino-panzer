9ca324204da1263d45eadb732ada31f686871c15
edumardo <aesalemolo@gmail.com>
2021-03-17 23:15:46 +0100

    ps2 values to vars

--- arduino-panzer.ino
+++ arduino-panzer.ino
@@ -12,29 +12,28 @@ PS2X ps2x;
 /*********************************************************************************************************************/
 void setup() {
 
-    Serial.begin(57600);
+    Serial.begin(115200);
     Serial.println("Inicializamos Serial");
 
+    int stickProperties[3] = {MIN_STICK_VALUE, MAX_STICK_VALUE, CENTER_STICK_VALUE};
+
     int turretRotationPinout[4] = {TB_TURRETROTATION_PWMA, TB_TURRETROTATION_AIN1, TB_TURRETROTATION_AIN2, TB_TURRETGUN_STBY};
-    turretRotation.begin(turretRotationPinout, MIN_STICK_VALUE, MAX_STICK_VALUE, CENTER_STICK_VALUE);
+    turretRotation.begin(turretRotationPinout, stickProperties);
     turretRotation.setMaxVoltagePercent(50);
     turretRotation.enableDebug("Turret rotation");
 
     int gunElevationPinout[4] = {TB_GUNELEVATION_PWMB, TB_GUNELEVATION_BIN1, TB_GUNELEVATION_BIN2, TB_TURRETGUN_STBY};
-    gunElevation.begin(gunElevationPinout, MIN_STICK_VALUE, MAX_STICK_VALUE, CENTER_STICK_VALUE);
+    gunElevation.begin(gunElevationPinout, stickProperties);
     gunElevation.setMaxVoltagePercent(50);
     gunElevation.enableDebug("Gun elevation");
 
-
     int leftMotorPinout[4] = {TB_DRIVE_PWMA, TB_DRIVE_AIN1, TB_DRIVE_AIN2, TB_DRIVE_STBY};
     int RightMotorPinout[4] = {TB_DRIVE_PWMB, TB_DRIVE_BIN1, TB_DRIVE_BIN2, TB_DRIVE_STBY};
-    int stickProperties[3] = {MIN_STICK_VALUE, MAX_STICK_VALUE, CENTER_STICK_VALUE};
     driveDirection.begin(leftMotorPinout, RightMotorPinout, stickProperties);
     driveDirection.invertYStick();
     driveDirection.setMaxVoltagePercentAnalogMove(50);
     driveDirection.setMaxVoltagePercentPadMove(40);
     driveDirection.enableDebug();
-
 
     delay(300);  //added delay to give wireless ps2 module some time to startup, before configuring it
     ps2xConfigError = ps2x.config_gamepad(PS2_PIN_CLK, PS2_PIN_CMD, PS2_PIN_SEL, PS2_PIN_DAT, PS2_PRESSURES, PS2_RUMBLE);
@@ -61,25 +60,34 @@ void loop() {
 
     ps2x.read_gamepad();
 
-    if ((ps2x.Analog(PSS_RX) != CENTER_STICK_VALUE) || (ps2x.Analog(PSS_RY) != CENTER_STICK_VALUE)) {
-        turretRotation.move(ps2x.Analog(PSS_RX));
-        gunElevation.move(ps2x.Analog(PSS_RY));
+    byte pss_rx         = ps2x.Analog(PSS_RX);
+    byte pss_ry         = ps2x.Analog(PSS_RY);
+    byte pss_lx         = ps2x.Analog(PSS_LX);
+    byte pss_ly         = ps2x.Analog(PSS_LY);
+    byte psab_pad_up    = ps2x.Analog(PSAB_PAD_UP);
+    byte psab_pad_down  = ps2x.Analog(PSAB_PAD_DOWN);
+    byte psab_pad_left  = ps2x.Analog(PSAB_PAD_LEFT);
+    byte psab_pad_right = ps2x.Analog(PSAB_PAD_RIGHT);
+
+    if ((pss_rx != CENTER_STICK_VALUE) || (pss_ry != CENTER_STICK_VALUE)) {
+        turretRotation.move(pss_rx);
+        gunElevation.move(pss_ry);
     }
     else {
         turretRotation.disableMotor();
         gunElevation.disableMotor();
     }
 
-    if(ps2x.Analog(PSS_LX) != CENTER_STICK_VALUE || ps2x.Analog(PSS_LY) != CENTER_STICK_VALUE) {
-        driveDirection.move(ps2x.Analog(PSS_LX), ps2x.Analog(PSS_LY));
+
+    if(pss_lx != CENTER_STICK_VALUE || pss_ly != CENTER_STICK_VALUE) {
+        driveDirection.move(pss_lx, pss_ly);
     }
     else if (ps2x.Button(PSB_PAD_UP) || ps2x.Button(PSB_PAD_RIGHT) || ps2x.Button(PSB_PAD_LEFT) || ps2x.Button(PSB_PAD_DOWN) ) {
-        driveDirection.move(ps2x.Analog(PSAB_PAD_UP), ps2x.Analog(PSAB_PAD_DOWN), ps2x.Analog(PSAB_PAD_LEFT), ps2x.Analog(PSAB_PAD_RIGHT));
+        driveDirection.move(psab_pad_up, psab_pad_down, psab_pad_left, psab_pad_right);
     }
     else {
         driveDirection.disableMotors();
     }
 
-    Serial.println("");
     delay(50);
 }
