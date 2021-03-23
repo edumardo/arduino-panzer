e19e883c748ef3ce9adb17df51c61306b7cdbe6c
edumardo <aesalemolo@gmail.com>
2021-03-24 00:28:16 +0100

    types and beautify

--- src/config.h
+++ src/config.h
@@ -1,42 +1,58 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* Center value of the transmitter stick */
+/* Transmitter stick */
+const int MIN_STICK_VALUE    =   0; 
 const int CENTER_STICK_VALUE = 128;
-
-/* Minimal value of the transmitter stick */
-const int MIN_STICK_VALUE = 0; 
-
-/* Maximal value of the transmitter stick */
-const int MAX_STICK_VALUE = 255;
+const int MAX_STICK_VALUE    = 255;
 
 /* PS2 pins */
-#define PS2_PIN_DAT A2
-#define PS2_PIN_CMD A1
-#define PS2_PIN_SEL A0
-#define PS2_PIN_CLK A3
-const bool PS2_PRESSURES = true;
-const bool PS2_RUMBLE    = true;
+const uint8_t PS2_PIN_DAT =   A2;
+const uint8_t PS2_PIN_CMD =   A1;
+const uint8_t PS2_PIN_SEL =   A0;
+const uint8_t PS2_PIN_CLK =   A3;
+const bool PS2_PRESSURES  = true;
+const bool PS2_RUMBLE     = true;
 
 /* TB6612FNG Drive pins */
-const int TB_DRIVE_PWMA =  4;
-const int TB_DRIVE_AIN2 = 23;
-const int TB_DRIVE_AIN1 = 25;
-const int TB_DRIVE_STBY = 27;
-const int TB_DRIVE_BIN1 = 29;
-const int TB_DRIVE_BIN2 = 31;
-const int TB_DRIVE_PWMB = 13;
+const uint8_t TB_DRIVE_APWM_PRESCALER =  1;
+const uint8_t TB_DRIVE_APWM           =  9;
+const uint8_t TB_DRIVE_AIN2           = 23;
+const uint8_t TB_DRIVE_AIN1           = 25;
+const uint8_t TB_DRIVE_STBY           = 27;
+const uint8_t TB_DRIVE_BIN1           = 29;
+const uint8_t TB_DRIVE_BIN2           = 31;
+const uint8_t TB_DRIVE_BPWM           = 10;
+const uint8_t TB_DRIVE_BPWM_PRESCALER =  1;
 
 /* TB6612FNG Turret rotation and gun elevation pins */
-const int TB_TURRETROTATION_PWMA = 11;
-const int TB_TURRETROTATION_AIN2 = 22;
-const int TB_TURRETROTATION_AIN1 = 24;
-const int TB_TURRETGUN_STBY      = 26;
-const int TB_GUNELEVATION_BIN1   = 28;
-const int TB_GUNELEVATION_BIN2   = 30;
-const int TB_GUNELEVATION_PWMB   = 12;
+const uint8_t TB_TURRETROTATION_PWM_PRESCALER =  1;
+const uint8_t TB_TURRETROTATION_PWM           = 11;
+const uint8_t TB_TURRETROTATION_IN2           = 22;
+const uint8_t TB_TURRETROTATION_IN1           = 24;
+const uint8_t TB_TURRETGUN_STBY               = 26;
+const uint8_t TB_GUNELEVATION_IN1             = 28;
+const uint8_t TB_GUNELEVATION_IN2             = 30;
+const uint8_t TB_GUNELEVATION_PWM             = 12;
+const uint8_t TB_GUNELEVATION_PWM_PRESCALER   =  1;
+
+/* TB6612FNG Smoke generator and fan pins */
+const uint8_t TB_SMOKE_GENERATOR_PWM_PRESCALER =  1;
+const uint8_t TB_SMOKE_GENERATOR_PWM           =  2;
+const uint8_t TB_SMOKE_GENERATOR_AIN2          = 32;
+const uint8_t TB_SMOKE_GENERATOR_AIN1          = 33;
+const uint8_t TB_SMOKE_GENERATORFAN_STBY       = 34;
+const uint8_t TB_SMOKE_FAN_IN1                 = 35;
+const uint8_t TB_SMOKE_FAN_IN2                 = 36;
+const uint8_t TB_SMOKE_FAN_PWM                 =  3;
+const uint8_t TB_SMOKE_FAN_PWM_PRESCALER       =  1;
+
+/* Dasmikro TBS Mini sound unit */
+const uint8_t TBSMINI_PROP1 = 49;
+const uint8_t TBSMINI_PROP2 = 51;
+const uint8_t TBSMINI_PROP3 = 53;
 
 /* Airsoft gun */
-const int AIRSOFT_FIRED_INTERRUPT_PIN = 18;
+const uint8_t AIRSOFT_FIRED_INTERRUPT_PIN = 18;
 
 #endif
