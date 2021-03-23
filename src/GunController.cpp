c7529b3a7cd8982a1ef209a80e93ee834c4cee80
edumardo <aesalemolo@gmail.com>
2021-03-24 00:32:18 +0100

    airsoft initial commit

---
+++ src/GunController.cpp
@@ -0,0 +1,96 @@
+#include "GunController.h"
+
+bool GunController::m_isFiring;
+bool GunController::m_isFired;
+byte GunController::m_airsoftCompleteInterruptPin;
+Timer<> * GunController::m_APTimer;
+uintptr_t GunController::m_taskAirsoftInterrupt;
+
+GunController::GunController() {
+
+    m_isFiring = false;
+    m_isFired = false;
+}
+
+void GunController::begin(Timer<> * APTimer, byte airsoftCompleteInterruptPin) {
+
+    m_APTimer = APTimer;
+    m_airsoftCompleteInterruptPin = airsoftCompleteInterruptPin;
+    pinMode(m_airsoftCompleteInterruptPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(m_airsoftCompleteInterruptPin), airsoftISR, CHANGE);
+}
+
+
+bool GunController::isFiring() {
+
+    return m_isFiring;
+}
+
+bool GunController::isFired() {
+
+    return m_isFired;
+}
+
+void GunController::readyToFire() {
+    m_isFired = false;
+}
+
+void GunController::fire() {
+
+    startAirsoftMotor();
+    enableAirsoftInterrupt();
+    m_taskAirsoftInterrupt = m_APTimer->in(5000, stopAirsoftMotor);
+    m_isFiring = true;
+    if (m_APTimer->empty()) { Serial.println("no active tasks"); }
+}
+
+void GunController::startAirsoftMotor() {
+
+    Serial.print("START startAirsoftMotor: ");
+    Serial.println(millis());
+    digitalWrite(40, HIGH);
+}
+
+bool GunController::stopAirsoftMotor(void *) {
+
+    Serial.print("STOP stopAirsoftMotor: ");
+    Serial.println(millis());
+    Serial.println();
+
+    digitalWrite(40, LOW);
+    m_APTimer->cancel(m_taskAirsoftInterrupt);
+
+    m_isFiring = false;
+    m_isFired = true;
+
+    return true;
+}
+void GunController::enableAirsoftInterrupt() {
+
+    //Serial.println("enableAirsoftInterrupt");
+    // activar pin de interrupción que lanza airsoftISR
+}
+void GunController::disableAirsoftInterrupt() {
+
+    //Serial.println("disableAirsoftInterrupt");
+    // desactivar pin de interrupción que lanza airsoftISR
+}
+
+void GunController::airsoftISR(void) {
+
+    disableAirsoftInterrupt();
+    stopAirsoftMotor((void *) 123);     // Random parameter
+    m_isFiring = false;
+    m_isFired = true;
+}
+
+void GunController::postFireActions() {
+
+    // si el tanque está parado, retroceso
+
+    // sonido de disparo
+
+    // humo de disparo
+
+    // luces de disparo
+}
---
+++ src/GunController.h
@@ -0,0 +1,37 @@
+#ifndef GUNCONTROLLER_H
+#define GUNCONTROLLER_H
+
+#include <arduino-timer.h>
+
+class GunController {
+
+    private:
+        static const int m_delayAirsoftMustBeComplete = 5000;        // Seconds when airsoft shoot must be complete
+        static bool m_isFiring;                                     // To know if gun is firing, i.e., running airsoft motor
+        static bool m_isFired;
+        static byte m_airsoftCompleteInterruptPin;
+        static Timer<> * m_APTimer;
+        static uintptr_t m_taskAirsoftInterrupt;
+
+        static void startAirsoftMotor();
+        static bool stopAirsoftMotor(void *);
+        static void enableAirsoftInterrupt();
+        static void disableAirsoftInterrupt();
+        static void airsoftISR(void);
+
+    public:
+        GunController();
+        static void begin(Timer<> * APTimer, byte AirsoftCompleteInterruptPin);
+        static bool isFiring();
+        static bool isFired();
+        static void readyToFire();
+        static void fire();
+        static void postFireActions();
+
+};
+
+//ISR(INT6_vect){
+//    GunController::airsoftISR();
+//}
+
+#endif
