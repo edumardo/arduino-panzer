d322465416317d5f22a9117036f40c3683b708ef
edumardo <aesalemolo@gmail.com>
2021-03-24 00:26:30 +0100

    config struct for pinout and types

--- src/DCMotorController.cpp
+++ src/DCMotorController.cpp
@@ -18,20 +18,24 @@ DCMotorController::DCMotorController() {
 /**
  *
  */
-void DCMotorController::begin(int pinMotor[4], int stickProperties[3]) {
+void DCMotorController::begin(DCMotorControllerConfig motorConfig, int stickProperties[3]) {
 
-    m_pwmPin = pinMotor[0];
-    m_in1Pin = pinMotor[1];
-    m_in2Pin = pinMotor[2];
-    m_standbyPin = pinMotor[3];
+    m_pwmPrescaler = motorConfig.pwmPrescaler;
+    m_pwmPin = motorConfig.pwmPin;
+    m_in1Pin = motorConfig.in1Pin;
+    m_in2Pin = motorConfig.in2Pin;
+    m_standbyPin = motorConfig.standbyPin;
+
     m_stickMinValue = stickProperties[0];
     m_stickMaxValue = stickProperties[1];
     m_stickCenterValue = stickProperties[2];
+
     pinMode(m_pwmPin, OUTPUT);
     pinMode(m_in1Pin, OUTPUT);
     pinMode(m_in2Pin, OUTPUT);
     pinMode(m_standbyPin, OUTPUT);
     disableMotor();
+    setPWMPrescaler(m_pwmPin, m_pwmPrescaler);
 }
 
 /**
@@ -42,7 +46,7 @@ void DCMotorController::begin(int pinMotor[4], int stickProperties[3]) {
  *
  * @param percent: (0,100] value.
  */
-void DCMotorController::setMaxVoltagePercent(int percent) {
+void DCMotorController::setMaxVoltagePercent(uint8_t percent) {
 
     m_maxVoltagePercent = (percent <= 0 || percent > 100) ? 100 : percent;
 }
@@ -69,6 +73,50 @@ void DCMotorController::disableMotor() {
 /**
  *
  */
+void DCMotorController::setPWMPrescaler(uint8_t pin, uint16_t prescale) {
+
+    byte mode;
+
+    if (pin == 9 || pin == 10) {
+        switch (prescale) {
+            case    1: mode = 0b001; break;
+            case    8: mode = 0b010; break;
+            case   32: mode = 0b011; break;
+            case   64: mode = 0b100; break;
+            case  128: mode = 0b101; break;
+            case  256: mode = 0b110; break;
+            case 1024: mode = 0b111; break;
+            default: return;
+        }
+    } else {
+        switch(prescale) {
+            case    1: mode = 0b001; break;
+            case    8: mode = 0b010; break;
+            case   64: mode = 0b011; break;
+            case  256: mode = 0b100; break;
+            case 1024: mode = 0b101; break;
+            default: return;
+        }
+    }
+
+    if (pin == 4 || pin == 13 ) {
+        TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else if (pin == 11 || pin == 12 ) {
+        TCCR1B = TCCR1B & 0b11111000 | mode;
+    } else if (pin == 9 || pin == 10 ) {
+        TCCR2B = TCCR2B & 0b11111000 | mode;
+    } else if (pin == 2 || pin ==  3 || pin == 5) {
+        TCCR3B  = TCCR3B & 0b11111000 | mode;
+    } else if (pin == 6 || pin ==  7 || pin == 8) {
+        TCCR4B  = TCCR4B & 0b11111000 | mode;
+    } else if (pin == 44 || pin == 45 || pin == 46) {
+        TCCR5B = TCCR5B & 0b11111000 | mode;
+    }
+}
+
+/**
+ *
+ */
 void DCMotorController::move(int stickValue) {
 
     if (stickValue == m_stickCenterValue) return;
@@ -87,7 +135,7 @@ void DCMotorController::move(int stickValue) {
         Serial.print(m_debugName);
         if (stickValue < m_stickCenterValue)
             Serial.print(" left");
-        else 
+        else
             Serial.print(" right");
         Serial.print("]");
         printDEC("pwmSpeedPercent", pwmSpeedPercent);
--- src/DCMotorController.h
+++ src/DCMotorController.h
@@ -3,6 +3,14 @@
 
 #include <Arduino.h>
 
+typedef struct DCMotorControllerConfig {
+    int pwmPrescaler;
+    uint8_t pwmPin;
+    uint8_t in1Pin;
+    uint8_t in2Pin;
+    uint8_t standbyPin;
+};
+
 class DCMotorController {
 
     private:
@@ -10,19 +18,21 @@ class DCMotorController {
         int m_stickMinValue;
         int m_stickMaxValue;
         int m_stickCenterValue;
-        int m_pwmPin;
-        int m_in1Pin;
-        int m_in2Pin;
-        int m_standbyPin;
-        int m_maxVoltagePercent;
+        int m_pwmPrescaler;
+        uint8_t m_pwmPin;
+        uint8_t m_in1Pin;
+        uint8_t m_in2Pin;
+        uint8_t m_standbyPin;
+        uint8_t m_maxVoltagePercent;
         String m_debugName;
         void printDEC(String label, int value);
+        void setPWMPrescaler(uint8_t pin, uint16_t prescale);
 
     public:
         DCMotorController();
-        void begin(int pinMotor[4], int stickProperties[3]);
+        void begin(DCMotorControllerConfig motorConfig, int stickProperties[3]);
         void move(int stickValue);
-        void setMaxVoltagePercent(int percent);
+        void setMaxVoltagePercent(uint8_t percent);
         void enableMotor();
         void disableMotor();
         void enableDebug(String debugName);
